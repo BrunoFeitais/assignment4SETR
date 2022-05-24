33a42d48d6eefc72c7bbcb12e8cc392d463d21ee
Bruno Feitais <brunofeitais2011@gmail.com>
2022-05-24 09:22:13 +0000

    ...

--- rt_05_dataflow_fifo_threads/build_nrf52840dk_nrf52840/zephyr/CMakeFiles/zephyr.dir/soc/arm/nordic_nrf/common/soc_nrf_common.S.obj.d
+++
@@ -1,8 +0,0 @@
-zephyr/CMakeFiles/zephyr.dir/soc/arm/nordic_nrf/common/soc_nrf_common.S.obj: \
- /home/pedreiras/Nordic/ncs/zephyr/soc/arm/nordic_nrf/common/soc_nrf_common.S \
- /home/pedreiras/dados/Pedagogic-stuff/Nordic/Demos/rt_05_dataflow_fifo_threads/build_nrf52840dk_nrf52840/zephyr/include/generated/autoconf.h \
- /home/pedreiras/Nordic/ncs/zephyr/include/toolchain/zephyr_stdint.h \
- /home/pedreiras/Nordic/ncs/zephyr/include/toolchain.h \
- /home/pedreiras/Nordic/ncs/zephyr/include/toolchain/gcc.h \
- /home/pedreiras/Nordic/ncs/zephyr/include/toolchain/common.h \
- /home/pedreiras/Nordic/ncs/zephyr/lib/libc/minimal/include/stdbool.h
